eaec3a993ebe61c4eb75094757090cd3e9a2d1df
Shittu Emmanuel <shem4soul@gmail.com>
2025-11-25 18:31:01 +0100

    added setInterval

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const correctAnswers = ['B', 'B', 'B', 'B'];
-
 const form = document.querySelector(".quiz-form");
+const result = document.querySelector('.result');
+
 
 form.addEventListener('submit', e => {
     e.preventDefault();
@@ -15,7 +16,20 @@ form.addEventListener('submit', e => {
         }
     });
 
-    console.log(score);
-    
+    //show result on page
+    scrollTo(0,0);
+   
+    result.classList.remove('d-none');
+
+   let output = 0;
+   const timer = setInterval(() => {
+    result.querySelector('span').textContent = `${output}%`;
+    if (output === score) {
+        clearInterval(timer);
+    }  else {
+        output++;
+    }
+    }, 10) ;
+     
 
 })
